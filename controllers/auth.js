c9149a14d40a5190e8e26a86bbaa4d162103476e
KARTHIKEYAN R <88583975+karthi0102@users.noreply.github.com>
2022-11-14 17:08:17 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,10 +32,7 @@ export const login = async(req,res)=>{
         if(!existinguser){
             return res.status(404).json({message:"User not found..."})
         }
-        const isPasswordCrt = await bcrypt.compare(password,existinguser.password)
-        if(!isPasswordCrt){
-            return res.status(400).json({message:"Invalid credentials"})
-        }
+     
         const token = jwt.sign({email:existinguser.email,id:existinguser._id},process.env.JWT_SECRET,{expiresIn:'1h'})
         res.status(200).json({result:existinguser,token})
     }catch(err){
